f368bbdd1ecde7fa5a66f79c1b96a86d658baf24
nasd4q <nasd4q@gmail.com>
2020-05-17 16:55:25 +0200

    done demo

---
+++ src/zoo/Hippo2.java
@@ -0,0 +1,29 @@
+/*
+ * Now Using Apache Commons Lang ToStringBuilder
+ */
+package zoo;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Hippo2 {
+    private String name;
+    private double weight;
+
+    public Hippo2(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(
+                this,
+                ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    public static void main(String[] args) {
+        Hippo2 h1 = new Hippo2("Harry", 3100);
+        System.out.println(h1); // Harry
+    }
+}
