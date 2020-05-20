e5dcf63a5b87bfd8eb3d457d57cf593ed9cc39c8
nasd4q <nasd4q@gmail.com>
2020-05-20 23:04:52 +0200

    added case of a method taking an Object

--- src/selection/TestClass.java
+++ src/selection/TestClass.java
@@ -13,19 +13,22 @@ public class TestClass{
 
     void probe(Long x) { System.out.println("In LONG"); } //4
 
+    void probe(Object o) { System.out.println("In Object"); } //4
+
+
     public static void main(String[] args){
-        //Integer a = 4; new TestClass().probe(a);
-            // Order of preference : probe(Integer), then probe(long) then probe(int...)
+        Integer a = 4; new TestClass().probe(a);
+            // Order of preference : probe(Integer), then probe(Object), then probe(long) then probe(int...)
             // DOES NOT COMPILE if only probe(Long) available :
             // "Error:(13, 46) java: incompatible types: java.lang.Integer cannot be converted to java.lang.Long
 
         //int b = 4; new TestClass().probe(b);
-            // Order of preference : probe(long), then probe(Integer), then probe(int...)
+            // Order of preference : probe(long), then probe(Integer), then probe(Object), then probe(int...)
             // DOES NOT COMPILE if only probe(Long) available :
             // Error:(18, 42) java: incompatible types: int cannot be converted to java.lang.Long
 
-        long l = 46; new TestClass().probe(l);
-            // Order of preference : probe(long), then probe(Long)
+        //long l = 46; new TestClass().probe(l);
+            // Order of preference : probe(long), then probe(Long), then probe(Object)
             // DOES NOT COMPILE otherwise.
     }
 }
