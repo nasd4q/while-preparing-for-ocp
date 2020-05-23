2984c6e9f637f623fd541ab3ee93c8b8b47516fe
nasd4q <nasd4q@gmail.com>
2020-05-23 06:48:35 +0200

    ArrayList(Collection) constructor with subtype - it works

---
+++ src/arraylist/ConstructorTest.java
@@ -0,0 +1,14 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConstructorTest {
+    public static void  main(String... args) {
+        List<String> strings = new ArrayList<String>();
+        strings.add("tweet");
+        List<Object> objects = new ArrayList<Object>(strings);
+        //  compiles because the constructor accepts a java.util.Collection<? extends E>
+        //      (where E here is Object)
+    }
+}
