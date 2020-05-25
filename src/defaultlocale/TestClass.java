42ff0280cb78507892ba062a4a4c593794e9efb6
nasd4q <nasd4q@gmail.com>
2020-05-25 21:10:06 +0200

    default test

---
+++ src/defaultlocale/TestClass.java
@@ -0,0 +1,10 @@
+package defaultlocale;
+
+import java.util.Locale;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println(locale); // fr_FR
+    }
+}
