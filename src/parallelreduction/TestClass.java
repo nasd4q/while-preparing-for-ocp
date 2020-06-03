395f3c4aefaf0cdcf59df21ddea1f3a0848fd7da
nasd4q <nasd4q@gmail.com>
2020-06-03 18:24:05 +0200

    screenshots

---
+++ src/parallelreduction/TestClass.java
@@ -0,0 +1,20 @@
+package parallelreduction;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestClass {
+    public static void main(String[] args) {
+        List<String> vals = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "a", "b", "c", "d", "e", "f", "g", "a", "b", "c", "d", "e", "f", "g", "a", "b", "c", "d", "e", "f", "g");
+        String join = vals.parallelStream()
+                .peek(System.out::println) //this shows how the elements are retrieved from the stream         
+                .reduce("_", (a, b) -> {
+                    System.out.println("reducing " + a + " and " + b + " Thread: " + Thread.currentThread().getName());
+                    return a.concat(b);
+                }, (a, b) -> {
+                    System.out.println("combining " + a + " and " + b + " Thread: " + Thread.currentThread().getName());
+                    return a.concat(b);
+                });
+        System.out.println(join);
+    }
+}
